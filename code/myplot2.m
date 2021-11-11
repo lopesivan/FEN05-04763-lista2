eec5a31fdce7d40dae4b5af4e4f2a7c3a0996b74
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-11 19:55:47 -0300

    Atualiza o arquivo code/myplot2.m

--- code/myplot2.m
+++ code/myplot2.m
@@ -1,8 +1,22 @@
-% Primeira tentativa
+% Name: code/myplot2.m
+% Description:  Segunda tentativa
 
+% Nossos vetores X e Y são compostos pelos valores de uma
+% progressão aritimética iniciada em -1, terminada em 1 com
+% uma razão de 0.00005.
+% a[inicial] = -1
+% a[final]   = 1
+% r = 0.5
 x = [-1:0.00005:1];
 y = [-1:0.00005:1];
-z = F6(x,y);
+z = arrayfun(@(x,y) F6(x,y), x, y)
+%   ^^^^^^^^ ^^^^   ^^^^^^   ^  ^
+%   |        |      |        |  |
+%   |        |      |        |  +-a segunda entrada vetorial
+%   |        |      |        +-a primeira entrada vetorial
+%   |        |      +-a função que será mapeada
+%   |        +-definição de entradas
+%   +-função mapeadora "MAP"
 
 figure
 hold on
