5ad828cf10b1f1c083906b056c5318f28f91b734
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-11 19:55:49 -0300

    Adiciona o arquivo code/l2/startup.m

---
+++ code/l2/startup.m
@@ -0,0 +1,12 @@
+%STARTUP Script to include GAOT directories in the matlabpath.
+%
+
+disp('Incluindo GAOT - Toolbox no path do Matlab')
+disp(' ')
+path(path,'gaot');
+disp(' ')
+disp('STARTUP CONCLUÍDO')
+disp(' ')
+
+
+
