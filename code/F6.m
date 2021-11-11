5629d84dded14f7b1d24fa4681cbb3b1903d9514
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-11 19:55:47 -0300

    Atualiza o arquivo code/F6.m

--- code/F6.m
+++ code/F6.m
@@ -1,8 +1,19 @@
-% function implementation
-%  Minha função implementada é a de um sistema de equações lineares
-% z = f(x,y)
+% Name: F6.m
+% Description: Implenatção da função F6
+%                                  2    2  2
+%                        sin(sqrt(x  + y ))  - 0.5
+%   z = F6(x,y) =  0.5 - -------------------------
+%                                      2    2  2
+%                         (1 + 0.001 (x  + y ))
+%
+%
+%   F6(0,0);
+%                   1.000000000
 function z = F6(x,y)
-    n = (sin(sqrt(x.^ 2 + y.^ 2)).^ 2 - 0.5);
-    d = (1 + 0.001 * (x.^ 2 + y.^ 2) ).^ 2;
-    z = 0.5 - n./d;
+    % inputs:
+    %   x - é um escalar
+    %   y - é um escalar
+    % output:
+    %   z - é um escalar
+	z = 0.5 - (sin(sqrt(x^2 + y^2))^2 - 0.5)/(1 + 0.001 * (x^2 + y^2) )^2;
 end
