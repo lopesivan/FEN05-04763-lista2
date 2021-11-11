a0c8a464259fd0facdd66f260660a5ee6ca475f9
Ivan Lopes <lopesivan.ufrj@gmail.com>
2021-11-11 19:55:47 -0300

    Atualiza o arquivo code/myplot1.m

--- code/myplot1.m
+++ code/myplot1.m
@@ -1,9 +1,22 @@
-% Primeira tentativa
+% Name: code/myplot1.m
+% Description:  Primeira tentativa
 
+% Nossos vetores X e Y são compostos pelos valores de uma
+% progressão aritimética iniciada em -100, terminada em 100 com
+% uma razão de 0.5.
+% a[inicial] = -100
+% a[final]   = 100
+% r = 0.5
 x = [-100:0.5:100];
 y = [-100:0.5:100];
-z = F6(x,y);
-
+z = arrayfun(@(x,y) F6(x,y), x, y)
+%   ^^^^^^^^ ^^^^   ^^^^^^   ^  ^
+%   |        |      |        |  |
+%   |        |      |        |  +-a segunda entrada vetorial
+%   |        |      |        +-a primeira entrada vetorial
+%   |        |      +-a função que será mapeada
+%   |        +-definição de entradas
+%   +-função mapeadora "MAP"
 figure
 plot3(x,y,z)
 xlabel('x')
